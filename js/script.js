cab9a6d5a800aac21e6be5bac3a7d95f817458fa
fuatsevinc <fuatsevinc@icloud.com>
2024-03-18 20:29:19 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1390,3 +1390,12 @@
 		}
 	});
 }());
+
+document.getElementById("openPopup").addEventListener("click", function() {
+	document.getElementById("popup").style.display = "block";
+  });
+  
+  document.getElementById("closePopup").addEventListener("click", function() {
+	document.getElementById("popup").style.display = "none";
+  });
+  
